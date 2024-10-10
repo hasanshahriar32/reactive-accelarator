6181d4e1ba689d71dbf2fb5b816751a02eb1afb4
MR. HASAN <hasanshahriar32@gmail.com>
2024-10-10 18:36:09 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import nextra from "nextra";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/reactive-accelarator",
+    basePath: "/reactive-accilarator",
     output: "export",
     reactStrictMode: true,
     images: {
